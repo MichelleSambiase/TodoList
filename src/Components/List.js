5e072fed9b685b4c2f8cfe7ac0dbb7d5ae2eb526
Michelle Sambiase <michellesambiase@gmail.com>
2021-04-14 20:47:05 -0300

    create scroll

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,72 +1,71 @@
-import React from 'react'
+import React from "react";
 
 import HighlightOffIcon from "@material-ui/icons/HighlightOff";
-import { makeStyles, Card, ListItem, IconButton } from '@material-ui/core';
+import { makeStyles, Card, ListItem, IconButton } from "@material-ui/core";
 
 const List = (props) => {
- 
-
-    const useStyles = makeStyles({
-        styleUL: {
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-          },
-          
+  const useStyles = makeStyles({
+    styleUL: {
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+    },
+    listContainer: {
+      maxHeight: "300px",
+      overflow:'auto',
+      width: "50%",
+      overflowX:"hidden",
+    },
     list: {
-        boxShadow: "0px 0px 10px 0px #b59696",
-        height: "300px",
-        width: "50%",
-        borderRadius: "10px",
-        padding: 0,
-        display: "flex",
-        alignItems: "center",
-        flexDirection: "column",
-        justifyContent: "center",
-        fontFamily: 'Sacramento , cursive',
-      },
-      
+      boxShadow: "0px 0px 10px 0px #b59696",
+      width:'100%',
+      height:'100%',
+      borderRadius: "10px",
+      padding: 0,
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+      justifyContent: "center",
+      fontFamily: "Sacramento , cursive",
+    },
     listItemStyle: {
-        width: "100%",
-      },
-      buttonDelete: {
-        display: " flex",
-        justifyContent: "space-between",
-        width: "100%",
-      },
-  
-      iconButton: {
-        color: "lightslategrey",
-      },
-    })
-    const classes = useStyles();
-    return (
-        <div>
-             <div className={classes.styleUL}>
-            <ul className={classes.list}>
-              {props.lista.map((item, index) => (
-                <Card
-                  className={classes.listItemStyle}
-                  button
-                  variant="outlined"
+      width: "100%",
+    },
+    buttonDelete: {
+      display: " flex",
+      justifyContent: "space-between",
+      width: "100%",
+    },
+    iconButton: {
+      color: "lightslategrey",
+    },
+  });
+  const classes = useStyles();
+  return (
+    <div>
+      <div className={classes.styleUL}>
+        <div className={classes.listContainer}>
+          <ul className={classes.list}>
+            {props.lista.map((item, index) => (
+              <Card className={classes.listItemStyle} button variant="outlined">
+                <ListItem
+                  onClick={() => props.handleClickEliminarTodoItem(index)}
+                  className={classes.buttonDelete}
+                  key={item}
                 >
-                  <ListItem
-                    onClick={() => props.handleClickEliminarTodoItem(index)}
-                    className={classes.buttonDelete}
-                    key={item}
-                  >
-                    {" "}
-                    {item}
-                    <IconButton className={classes.iconButton}>
-                      <HighlightOffIcon />
-                    </IconButton>{" "}
-                  </ListItem>
-                </Card>
-              ))}
-            </ul>
-          </div>
+                  {" "}
+                  {item}
+                  <IconButton className={classes.iconButton}>
+                    <HighlightOffIcon />
+                  </IconButton>{" "}
+                </ListItem>
+              </Card>
+            ))}
+          </ul>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default List
+export default List;
